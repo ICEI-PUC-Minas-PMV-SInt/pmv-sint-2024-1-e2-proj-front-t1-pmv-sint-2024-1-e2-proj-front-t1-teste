f16a25569bb16ab3d6df4bc34d34dbf4689aedba
Harison Herman Silva <harisonppgee@gmail.com>
2024-05-31 21:53:56 -0300

    Agenda vinculada ao usuário logado

--- src/teste-app/src/App.js
+++ src/teste-app/src/App.js
@@ -1,52 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button/Button';
-import Formulário from './components/Formulário/Formulário';
 import {useState} from 'react';
 import Tabela from './components/Tabela/Tabela';
 
 function App() {
-  const imgInit = JSON.parse(localStorage.getItem('urlImg')) || ''; 
+  const listaUsuários = JSON.parse(localStorage.getItem("usuários")) || [];
+
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
-  const [urlImg, setUrlImg] = useState(imgInit);
+  const [senha, setSenha] = useState('');
 
-  function setImage(){
-    const urlFlor = "https://img.freepik.com/fotos-gratis/flores-roxas-em-um-vaso_1340-25662.jpg";
-    setUrlImg(urlFlor);
-    localStorage.setItem('urlImg', JSON.stringify(urlFlor));
+  const [usuárioInformado, setUsuárioInformado] = useState('');
+  const [senhaInformada, setSenhaInformada] = useState('');
+
+  const [currentUser, setCurrentUser] = useState({
+    "username": "",
+    "senha": "",
+    "email": ""
+  });
+
+  function addUser(e){
+    e.preventDefault();
+    const usuário = {
+      username,
+      email,
+      senha
+    };
+    listaUsuários.push(usuário);
+    localStorage.setItem("usuários", JSON.stringify(listaUsuários));
+    setEmail("");
+    setUsername("");
+    setSenha("");
   }
 
-  function handleUser(e){
-    setUsername(e.target.value);
-  }
-
-  function handleEmail(){
-
+  function getUser(e){
+    e.preventDefault();
+    const user = listaUsuários.find((valor, index, obj) => 
+        valor.username === usuárioInformado && valor.senha === senhaInformada) || {
+          "username": "",
+          "senha": "",
+          "email": ""
+        };
+    setCurrentUser(user);
   }
 
   return (
     <div className="App">
-      {username? <span>Bem-vindo, {username}!</span>: <></>}
-      {/* <div className="container_1">
-        <Formulário
-          formTitle={"Novo usuário"}
-          label={"Username"}
-          btnName={"OK"}
-          onSubmit={handleUser}
-        />
+      {currentUser.username? <span>Bem-vindo, {currentUser.username}!</span>: <></>}
+      <h1>Agenda</h1>
+      <Tabela currentUser={currentUser} />
+      <div className="container_1">
+        <form>
+          <label>Username: </label>
+          <input type="text" placeholder='Username' onChange={e => setUsername(e.target.value)} value={username}></input> <br></br>
+          <label>Senha: </label>
+          <input type='text' placeholder='Senha' onChange={e => setSenha(e.target.value)} value={senha}></input> <br></br>
+          <label>E-mail: </label>
+          <input type='text' placeholder='E-mail' onChange={e => setEmail(e.target.value)} value={email} ></input> <br></br>
+          <button onClick={e => addUser(e)}>OK</button>
+        </form>
       </div>
+
       <div className="container_2">
-        <Formulário
-          formTitle={"Assine nossa Newsletter"}
-          label={"E-mail"}
-          btnName={"OK"}
-          onSubmit={handleEmail}
-        />
-      </div> */}
-      <img src={urlImg} alt="Imagem de flor" loading='lazy'></img>
-      <h1 onClick={() => setImage() }>Agenda</h1>
-      <Tabela />
+        <h2>Login</h2>
+        <form>
+          <label>Username: </label>
+          <input type="text" placeholder='Username' onChange={e => setUsuárioInformado(e.target.value)}></input> <br></br>
+          <label>Senha: </label>
+          <input type='password' placeholder='Senha' onChange={e => setSenhaInformada(e.target.value)}></input> <br></br>
+          <button onClick={e => getUser(e)}>OK</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/teste-app/src/components/Tabela/Tabela.js
+++ src/teste-app/src/components/Tabela/Tabela.js
@@ -1,52 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Tabela.css'
 
-function Tabela(){
-    const history = JSON.parse(localStorage.getItem("compromissos")) || [];
+function Tabela({ currentUser }) {
+    const history = JSON.parse(localStorage.getItem("compromissos-" + currentUser.username)) || [];
     const [compromissos, setCompromissos] = useState(history);
-
-    const getFromStorage = (keylocalstorage) => {
-        let aux;
-        switch(keylocalstorage){
-            case "meuitem":
-                aux = JSON.parse(localStorage.getItem(keylocalstorage)) || 
-                {
-                    "data": "",
-                    "pessoa_responsavel": "",
-                };
-                break;
-            case "sanduíche":
-                aux = JSON.parse(localStorage.getItem(keylocalstorage)) ||
-                {
-                    "ID": 0,
-                    "apelido": "",
-                };
-                break;
-            default: 
-                aux = {};
-        }
-        
-        return aux;
-    };
-
-    const setObjectDate = () => {
-        const aux =  getFromStorage("meuitem");
-        aux.data = "17/05";
-        localStorage.setItem("meuitem", JSON.stringify(aux));
-    }
-
-    const setObjectPerson = () => {
-        const aux =  getFromStorage("meuitem");
-        aux.pessoa_responsavel = "Harison";
-        localStorage.setItem("meuitem", JSON.stringify(aux));
-    }
+    useEffect(() => {
+        setCompromissos(history);
+    }, [currentUser]);
 
     const handleSetCompromisso = (evt, dia) => {
         const textoCompromisso = evt.target.value;
         const aux = compromissos.slice(0);
         aux[dia] = textoCompromisso;
         setCompromissos(aux);
-        localStorage.setItem("compromissos", JSON.stringify(aux));
+        localStorage.setItem("compromissos-" + currentUser.username, JSON.stringify(aux));
     };
 
     const dias_semana = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"];
@@ -56,16 +23,16 @@ function Tabela(){
             <div className='container'>
                 {dias_semana.map((dia) => <span>{dia}</span>)}
                 {
-                    dias_mes.map(dia => 
+                    dias_mes.map(dia =>
                         <div>
                             <span>{dia}</span>
-                            <br/>
-                            <textarea cols="15" rows="3" value={compromissos[dia]} 
-                                        onChange={(evt) => {handleSetCompromisso(evt, dia)}}></textarea>
+                            <br />
+                            <textarea cols="15" rows="3" value={compromissos[dia]}
+                                onChange={(evt) => { handleSetCompromisso(evt, dia) }}></textarea>
                         </div>
                     )
                 }
-                
+
             </div>
         </>
     );
